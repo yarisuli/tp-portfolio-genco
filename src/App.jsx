38c3aea2bac05f7d92741a6a2cc3ddaa50f06b29
Iara Genco <148562678+yarisuli@users.noreply.github.com>
2025-11-26 22:12:44 -0300

    acerca de mi hecho

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from './components/Header'
 import HeroSection from './components/HeroSection'
+import AboutSection from './components/AboutSection'
 
 function App() {
   return (
     <div className="app">
       <Header />
       <HeroSection />
+      <AboutSection />
     </div>
   )
 }
---
+++ src/components/AboutSection.jsx
@@ -0,0 +1,29 @@
+import './AboutSection.css'
+
+function AboutSection() {
+  return (
+    <section className="about-section">
+      <div className="about-container">
+        <h2 className="about-title">Acerca de mi</h2>
+        <div className="about-content">
+          <div className="about-list">
+            <h3 className="about-subtitle">Hola Mundo!</h3>
+            <ul className="about-items">
+              <li>Programo desde que tengo 7 años</li>
+              <li>Busco aprender nuevas tecnologias</li>
+              <li>Apasionada por el descubrimiento</li>
+              <li>Velo por la estructuracion y optimizacion</li>
+              <li>Aprendi a ser disciplinada a traves<br />de la musica</li>
+            </ul>
+          </div>
+          <div className="about-image-container">
+            <img src="/bateria.png" alt="Batería" className="about-image" />
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default AboutSection
+
